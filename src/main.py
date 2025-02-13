f12ba43d7d8e0a4e3c6ca051915e0507ed0daf4f
Fernando Dorantes <fdorantesm@gmail.com>
2025-02-13 14:21:10 -0600

    chore: initial commit 🚀

---
+++ src/main.py
@@ -0,0 +1,329 @@
+import datetime
+import os
+import re
+from flask import Blueprint, Flask, json, make_response, request, jsonify
+from dotenv import load_dotenv
+from lib.cognito import client, hash
+
+
+load_dotenv()
+
+CLIENT_ID = os.getenv('COGNITO_CLIENT_ID')
+CLIENT_SECRET = os.getenv('COGNITO_CLIENT_SECRET')
+
+app = Flask(__name__)
+port = os.getenv('PORT')
+
+api = Blueprint('api', __name__, url_prefix='/api')
+
+@api.route('/auth/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    print
+    username = data['username']
+    password = data['password']
+    try:
+        response = client.initiate_auth(
+            ClientId=os.getenv('COGNITO_CLIENT_ID'),
+            AuthFlow='USER_PASSWORD_AUTH',
+            AuthParameters={
+                'USERNAME': username,
+                'PASSWORD': password,
+                'SECRET_HASH': hash(username, CLIENT_ID, CLIENT_SECRET)
+            }
+        )
+        
+        return jsonify({
+            'session': response['Session']
+        })
+    except client.exceptions.NotAuthorizedException as e:
+        return make_response(jsonify({
+            'message': 'Invalid username or password'
+        }), 401)
+        
+@api.route('/auth/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    last_password_change = ""
+    scopes = {
+        'payments': False,
+    }
+    
+    attributes = [
+        {
+            'Name': 'email',
+            'Value': username
+        },
+        {
+            'Name': 'custom:last_password_change',
+            'Value': str(last_password_change)
+        },
+        {
+            'Name': 'custom:scopes',
+            'Value': json.dumps(scopes)
+        }
+    ]
+    
+    try:
+        client.sign_up(
+            ClientId=os.getenv('COGNITO_CLIENT_ID'),
+            Username=username,
+            Password=password,
+            SecretHash=hash(username, CLIENT_ID, CLIENT_SECRET),
+            UserAttributes=attributes,
+        )
+        
+        client.admin_confirm_sign_up(
+            UserPoolId=os.getenv('COGNITO_POOL_ID'),
+            Username=username
+        )
+        
+        client.admin_update_user_attributes(
+            UserPoolId=os.getenv('COGNITO_POOL_ID'),
+            Username=username,
+            UserAttributes=[
+                {
+                    'Name': 'email_verified',
+                    'Value': 'true'
+                },
+            ]
+        )
+        
+        return jsonify({
+            'message': 'User created'
+        })
+    except client.exceptions.UsernameExistsException as e:
+        print(e)
+        return make_response(jsonify({
+            'message': e.response['Error']['Message']
+        }), 409)
+
+@api.route('/auth/validate-otp', methods=['POST'])
+def validate_otp():
+    try:
+        data = request.get_json()
+        username = data['username']
+        code = data['code']
+        session = data['session']
+        response = client.respond_to_auth_challenge(
+            ClientId=os.getenv('COGNITO_CLIENT_ID'),
+            ChallengeName='EMAIL_OTP',
+            Session=session,
+            ChallengeResponses={
+                'USERNAME': username,
+                'EMAIL_OTP_CODE': code,
+                'SECRET_HASH': hash(username, CLIENT_ID, CLIENT_SECRET),
+            }
+        )
+        
+        return jsonify({
+            'message': 'Ok',
+            'data': {
+                'accessToken': response['AuthenticationResult']['AccessToken'],
+                'refreshToken': response['AuthenticationResult']['RefreshToken'],
+                'idToken': response['AuthenticationResult']['IdToken'],
+                'expiresIn': response['AuthenticationResult']['ExpiresIn'],
+            }
+        })
+    
+    except client.exceptions.NotAuthorizedException as e:
+        return make_response(jsonify({
+            'message': 'Invalid code or session'
+        }), 401)
+    
+    
+
+@api.route('/auth/me', methods=['GET'])
+def me():
+    auth_header = request.headers.get('Authorization')
+    token = re.match(r'Bearer (.*)', auth_header)[1] if auth_header else 'Bearer'
+    
+    try:
+        response = client.get_user(
+            AccessToken=token
+        )
+        
+        return jsonify({
+            'data': {
+                'username': response['UserAttributes'][0]['Value'],
+                'scopes': json.loads(response['UserAttributes'][2]['Value']),
+                'last_password_change': response['UserAttributes'][3]['Value']
+            }
+        })
+    except client.exceptions.NotAuthorizedException as e:
+        return make_response(jsonify({
+            'message': 'Invalid token'
+        }), 401)
+
+@api.route('/auth/privacy-policy', methods=['POST'])
+def privacy_policy():
+    return jsonify({
+        'message': 'Ok'
+    })
+    
+@api.route('/auth/send-password-recovery-link', methods=['POST'])
+def send_password_recovery_link():
+    data = request.get_json()
+    username = data['username']
+    
+    try:
+        response = client.forgot_password(
+            ClientId=os.getenv('COGNITO_CLIENT_ID'),
+            Username=username,
+            SecretHash=hash(username, CLIENT_ID, CLIENT_SECRET),
+        )
+        
+        user = client.admin_get_user(
+            UserPoolId=os.getenv('COGNITO_POOL_ID'),
+            Username=username,
+        )
+        
+        print(user)
+                
+        return jsonify({
+            'message': 'Password recovery link sent'
+        })
+    except client.exceptions.UserNotFoundException as e:
+        return make_response(jsonify({
+            'message': 'User not found'
+        }), 404)
+    except client.exceptions.InvalidParameterException as e:
+        print(e)
+        return make_response(jsonify({
+            'message': 'Invalid username'
+        }), 400)
+    except client.exceptions.LimitExceededException as e:
+        return make_response(jsonify({
+            'message': 'Limit exceeded'
+        }), 429)
+    except Exception as e:
+        return make_response(jsonify({
+            'message': str(e)
+        }), 500)
+    
+@api.route('/auth/password', methods=['POST'])
+def password():
+    data = request.get_json()
+    password = data['password']
+    auth_header = request.headers.get('Authorization')
+    token = re.match(r'Bearer (.*)', auth_header)[1] if auth_header else None
+    
+    if not token:
+        return make_response(jsonify({
+            'message': 'Invalid token'
+        }), 401)
+        
+    try:
+        client.change_password(
+            PreviousPassword=password,
+            ProposedPassword=password,
+            AccessToken=token
+        )
+        
+        client.update_user_attributes(
+            AccessToken=token,
+            UserAttributes=[
+                {
+                    'Name': 'custom:last_password_change',
+                    'Value': str(datetime.now())
+                }
+            ]
+        )
+        
+        return jsonify({
+            'message': 'Password changed'
+        })
+        
+    except client.exceptions.NotAuthorizedException as e:
+        return make_response(jsonify({
+            'message': e.response['Error']['Message']
+        }), 401)
+        
+@app.route('/auth/privacy-policy', methods=['POST'])
+def privacy_policy():
+    
+    auth_header = request.headers.get('Authorization')
+    token = re.match(r'Bearer (.*)', auth_header)[1] if auth_header else None
+    
+    if not token:
+        return make_response(jsonify({
+            'message': 'Invalid token'
+        }), 401)
+        
+    try:
+        client.update_user_attributes(
+            AccessToken=token,
+            UserAttributes=[
+                {
+                    'Name': 'custom:privacy_policy',
+                    'Value': 'true'
+                }
+            ]
+        )
+        
+        return jsonify({
+            'message': 'Privacy policy accepted'
+        })
+        
+    except client.exceptions.NotAuthorizedException as e:
+        return make_response(jsonify({
+            'message': e.response['Error']['Message']
+        }), 401)
+
+@app.route('/info', methods=['GET'])
+def info():
+    try:
+        response = client.describe_user_pool(
+            UserPoolId=os.getenv('COGNITO_POOL_ID')
+        )
+        
+        return jsonify({
+            'data': response['UserPool']
+        })
+    except Exception as e:
+        return jsonify({
+            'message': e.response['Error']['Message']
+        })
+        
+@app.route('/reset', methods=['GET'])
+def reset():
+    try:
+        response = client.list_users(
+            UserPoolId=os.getenv('COGNITO_POOL_ID')
+        )
+        
+        for user in response['Users']:
+            client.admin_delete_user(
+                UserPoolId=os.getenv('COGNITO_POOL_ID'),
+                Username=user['Username']
+            )
+        
+        return jsonify({
+            'message': 'Ok'
+        })
+    except Exception as e:
+        return jsonify({
+            'message': e.response['Error']['Message']
+        })
+    
+@api.route('/users', methods=['GET'])
+def users():
+    try:
+        response = client.list_users(
+            UserPoolId=os.getenv('COGNITO_POOL_ID')
+        )
+        
+        return jsonify({
+            'data': response['Users']
+        })
+
+    except Exception as e:
+        return jsonify({
+            'message': e.response['Error']['Message']
+        })
+    
+app.register_blueprint(api)
+
+app.run(host="0.0.0.0", port=9001, debug=True)
